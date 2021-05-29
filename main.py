10ef2b548cee7053677c0f611f17dda7e62b62e0
mike213112 <mikeramirez-14-31@hotmail.com>
2021-05-29 10:02:59 -0600

    reconocimiento de rostro

--- main.py
+++ main.py
@@ -1,47 +1,74 @@
 from tkinter import *
 from tkinter import messagebox
 import platform
-from Database.database import *
-import cv2
-import os
-import imutils
+from Funciones.reconocimiento import reconocerRostro
 
+# Variables
 SYSTEM = platform.system()
 
-windows = Tk()
-windows.title('Deteccion de Emociones')
+ventana = Tk()
+ventana.title("Reconocimiento de Gestos")
 
-CARPETAFELICIDAD = os.mkdir(mostrar + "/Fotos/Felicidad")
-CARPETAENOJO = os.mkdir(mostrar + "/Fotos/Enojo")
-CARPETAMIEDO = os.mkdir(mostrar + "/Fotos/Miedo")
-CARPETATRISTEZA = os.mkdir(mostrar + "/Fotos/Tristeza")
-CARPETASORPRESA = os.mkdir(mostrar + "/Fotos/Sorpresa")
-CARPETAASCO = os.mkdir(mostrar + "/Fotos/Asco")
 
-"""
-    Funcion de entramiento
-"""
-def entramiento():
-    pass
+# Function to center window
+def center(toplevel):
+    toplevel.update_idletasks()
+    w = toplevel.winfo_screenwidth()
+    h = toplevel.winfo_screenheight()
+    size = tuple(int(_) for _ in toplevel.geometry().split('+')[0].split('x'))
+    x = w/2 - size[0]/2
+    y = h/2 - size[1]/2 
+    toplevel.geometry("%dx%d+%d+%d" % (size + (x, y)))
 
-"""
-    funtion for detect system
-"""
-def WindowsPlatform(system):
-    if system == 'Windows':
-        windows.attributes('-fullscreen', True)
-        windows.mainloop()
 
-def LinuxPlatform(system):
+def other():
+    ventana2 = Tk()
+    ventana2.title("")
+    ventana2.geometry("200x200")
+
+    # Label
+    label = Label(ventana2,text="Nombre")
+    label.place(x=10,y=50)
+
+    # Input
+    input = Entry(ventana2)
+    input.place(x=10, y=70)
+
+    # Funtions
+    def pasarNombre():
+        name = input.get()
+        #print(name)    
+
+        ventana2.destroy()
+
+        reconocerRostro(name)
+
+    # Boton
+    boton = Button(ventana2, text="Enviar", command=pasarNombre)
+    boton.place(x=70,y=100)
+
+    center(ventana2)
+    ventana2.mainloop()
+
+
+def WindowsSystem():
+    if SYSTEM == 'Windows':
+        ventana.attributes('')
+
+        ventana.mainloop()
+
+
+def LinuxSystem():
     if SYSTEM == 'Linux':
-        windows.attributes('-zoomed', True)
+        ventana.attributes('-zoomed', True)
 
-        boton = Button(windows, text="Entrenamiendo de la IA", font=('Arial',18),fg="red")
-        boton.place(x=50, y=100)
+        boton = Button(ventana, text="Entrenamiento", font=('Arial',12), width=30, height=30, command=other)
+        boton.place(x=30, y=50)
 
-        windows.mainloop()
+        ventana.mainloop()
 
 
-#WindowsPlatform(SYSTEM)
-LinuxPlatform(SYSTEM)
-mostrar = os.getcwd()
+if SYSTEM == 'Linux':
+    LinuxSystem()
+else:
+    WindowsSystem()
